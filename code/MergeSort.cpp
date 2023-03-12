ee6677cd9f38b5f57d854064d4a8f3029aeee25d
lbngyn <119998622+lbngyn@users.noreply.github.com>
2023-03-13 00:01:17 +0700

    Update MergeSort.cpp

--- code/MergeSort.cpp
+++ code/MergeSort.cpp
@@ -3,10 +3,6 @@
 using namespace std;
 const int MAXN = 1e6;
 
-#include <windows.h>
-double PCFreq = 0.0;
-__int64 CounterStart = 0;
-
 void merge(double array[], int const left, int const mid,
            int const right)
 {
@@ -67,31 +63,11 @@ void mergeSort(double array[], int const begin, int const end)
     merge(array, begin, mid, end);
 }
 
-void StartCounter()
-{
-    LARGE_INTEGER li;
-    if (!QueryPerformanceFrequency(&li))
-        cout << "QueryPerformanceFrequency failed!\n";
-
-    PCFreq = double(li.QuadPart) / 1000.0;
-
-    QueryPerformanceCounter(&li);
-    CounterStart = li.QuadPart;
-}
-
-double GetCounter()
-{
-    LARGE_INTEGER li;
-    QueryPerformanceCounter(&li);
-    return double(li.QuadPart - CounterStart) / PCFreq;
-}
-
 int main()
 {
     ios_base::sync_with_stdio(0);
     cin.tie(0);
     cout.tie(0);
-    StartCounter();
 
     double arr[MAXN + 5];
     int n = MAXN;
@@ -102,10 +78,12 @@ int main()
         ifstream fi(fileName);
         for (int i = 0; i < n; i++)
             fi >> arr[i];
-        auto start = GetCounter();
+        auto start = std::chrono::high_resolution_clock::now();
         mergeSort(arr, 0, n - 1);
-        auto end = GetCounter();
-        cout << "The runtime of testcase " << i << " is: " << (double)(end - start) << '\n';
+        auto end = std::chrono::high_resolution_clock::now();
+        chrono::duration<double> elapsed = end - start;
+        chrono::milliseconds d = chrono::duration_cast< chrono::milliseconds >( elapsed );
+        cout << elapsed.count() << endl ;
         fi.close();
     }
 
